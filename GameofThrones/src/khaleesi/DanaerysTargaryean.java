c37e3cbecf1db245ebee70488fd4786ff1149935
LakshmanGottapu <sailakshman120@gmail.com>
2022-03-08 11:25:41 +0530

    add

--- GameofThrones/src/khaleesi/DanaerysTargaryean.java
+++ GameofThrones/src/khaleesi/DanaerysTargaryean.java
@@ -2,7 +2,7 @@ package khaleesi;
 
 public class DanaerysTargaryean {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Dracaryeus");
 	}
