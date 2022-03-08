fb62f02f3e87b43ccf9840d9bb5c07a68d03d535
LakshmanGottapu <sailakshman120@gmail.com>
2022-03-08 11:23:04 +0530

    adding gameofthrones to got repository

---
+++ GameofThrones/src/khaleesi/DanaerysTargaryean.java
@@ -0,0 +1,10 @@
+package khaleesi;
+
+public class DanaerysTargaryean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Dracaryeus");
+	}
+
+}
